6a42ab9042d99ac6848eb8aa8d786dafb81d92ae
Tito Arevalo <titotono00@hotmail.es>
2022-01-16 12:55:44 -0600

    MailTest

---
+++ plugins/mail.php
@@ -0,0 +1,19 @@
+<?php 
+    if(isset($_POST['enviar'])){
+        if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])){
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $message = $_POST['message'];
+
+            $header = "From: noreply@gmail.com" . "/r/n";
+            $header.= "Reply: noreply@gmail.com" . "/r/n";
+            $header.= "X-Mailer: PHP/". phpversion();
+
+            $mail = mail($email, $name, $message, $header);
+
+            if ($mail){
+                echo "<h4>Enviado<h4>";
+            }
+        }
+    }
+?>
